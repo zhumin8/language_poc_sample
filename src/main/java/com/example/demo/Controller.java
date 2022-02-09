97b2373af46df3a9b1267b56f6ffe2928bfb3925
Min Zhu <zhumin@google.com>
2022-02-09 00:10:43 +0000

    language starter clientlib sample.

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import java.io.IOException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,24 @@ public class Controller {
   @GetMapping("/")
   String sanityCheck() {
     return "hello world!!";
+  }
+
+  @GetMapping("/nl")
+  void languageClientLib() throws IOException {
+    // Instantiates a client
+    LanguageServiceClient language = LanguageServiceClient.create();
+
+    // The text to analyze
+    String[] texts = {"I love this!", "I hate this!"};
+    for (String text : texts) {
+      Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+      // Detects the sentiment of the text
+      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+
+      System.out.printf("Text: \"%s\"%n", text);
+      System.out.printf(
+          "Sentiment: score = %s, magnitude = %s%n",
+          sentiment.getScore(), sentiment.getMagnitude());
+    }
   }
 }
