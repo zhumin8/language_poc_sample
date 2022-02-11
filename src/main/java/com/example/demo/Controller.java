08f8bd70c3fd603f104215803290a07c5117c341
Min Zhu <zhumin@google.com>
2022-02-11 16:25:05 +0000

    cl-lib usage, add analyze entities.

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.Entity;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,10 @@ public class Controller {
     LanguageServiceClient language = LanguageServiceClient.create();
 
     // The text to analyze
-    String[] texts = {"I love this!", "I hate this!"};
+    String[] texts = {"I love this!", "I hate this!",
+        "Google, headquartered in Mountain View (1600 Amphitheatre Pkwy, Mountain View, CA 940430),"
+            + " unveiled the new Android phone for $799 at the Consumer Electronic Show. "
+            + "Sundar Pichai said in his keynote that users love their new Android phones."};
     for (String text : texts) {
       Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
       // Detects the sentiment of the text
@@ -32,6 +37,13 @@ public class Controller {
       System.out.printf(
           "Sentiment: score = %s, magnitude = %s%n",
           sentiment.getScore(), sentiment.getMagnitude());
+
+      List<Entity> entities = language.analyzeEntities(doc).getEntitiesList();
+      System.out.println("Analyze Entities: ");
+      entities.forEach(x -> System.out.println("Entity: " + x.getName() +
+          ", Salience: " + x.getSalience() +
+          ", Sentiment: " + x.getSentiment() +
+          ", Mention counts: " + x.getMentionsCount()));
     }
   }
 }
