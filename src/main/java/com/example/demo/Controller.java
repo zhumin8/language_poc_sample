1317d3440a7c7bae3f727b24da729fdda3d80772
Min Zhu <zhumin@google.com>
2022-07-26 17:31:41 +0000

    added actuator for testing. moved settings print to separate method.

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,18 +1,26 @@
 package com.example.demo;
 
+import com.google.api.gax.core.ExecutorProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.core.InstantiatingExecutorProvider;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.Entity;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.LanguageServiceSettings;
 import com.google.cloud.language.v1.Sentiment;
+import com.google.cloud.spring.core.Credentials;
 import com.google.cloud.spring.core.UserAgentHeaderProvider;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,12 +41,9 @@ public class Controller {
     return "hello world!!";
   }
 
-  /**
-   * Usage with autoconfig bean.
-   * should add settings and credentials to show difference from client library directly
-   */
-  @GetMapping("/autoconfig")
-  void withAutoConfigClient() throws IOException {
+  @GetMapping("/printsettings")
+  void printAutoconfigSettings() throws IOException {
+
     String quotaProjectId = this.autoClient.getSettings().getQuotaProjectId();
     Map<String, String> headers = this.autoClient.getSettings().getHeaderProvider().getHeaders();
     Map<String, List<String>> requestMetadata = this.autoClient.getSettings().getCredentialsProvider().getCredentials().getRequestMetadata();
@@ -52,13 +57,25 @@ public class Controller {
     headers.forEach((x, y) -> {
       LOGGER.info(x + ": " + y + "\n");
     });
+    InstantiatingExecutorProvider executorProvider = (InstantiatingExecutorProvider) this.autoClient.getSettings()
+        .getBackgroundExecutorProvider();
+    LOGGER.info("executorProvider thread count: " + executorProvider.getExecutorThreadCount());
+  }
 
+  /**
+   * Usage with autoconfig bean.
+   * should add settings and credentials to show difference from client library directly
+   */
+  @GetMapping("/autoconfig")
+  void withAutoConfigClient() {
 
-     for (String text : this.texts) {
+    for (String text : this.texts) {
       Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
       // Detects the sentiment of the text
-      Sentiment sentiment = this.autoClient.analyzeSentiment(doc).getDocumentSentiment();
-
+       Sentiment sentiment = this.autoClient.analyzeSentiment(doc).getDocumentSentiment();
+       // for (int i = 0; i < 20 ; i++) {
+       //  this.autoClient.analyzeSentiment(doc).getDocumentSentiment();
+       //   }
       LOGGER.info(String.format("Text: \"%s\"%n", text));
        LOGGER.info(String.format(
           "Sentiment: score = %s, magnitude = %s%n",
@@ -71,27 +88,49 @@ public class Controller {
           ", Sentiment: " + x.getSentiment() +
           ", Mention counts: " + x.getMentionsCount()));
     }
+     this.autoClient.close();
   }
 
+  @Value("${clientlib.credentials.location}")
+  private Resource location;
+
+  // @Value("${clientlib.quotaProjectId}")
+  // private String quotaProjectId;
   /**
    * Usage with client library directly.
    */
   @GetMapping("/nl")
   void languageClientLib() throws IOException {
+
+    Credentials credentials = new Credentials("https://www.googleapis.com/auth/cloud-language");
+    credentials.setLocation(location);
+
+    FixedCredentialsProvider fixedCredentialsProvider = FixedCredentialsProvider.create(
+        GoogleCredentials.fromStream(credentials.getLocation().getInputStream())
+            .createScoped(credentials.getScopes()));
+
     LanguageServiceSettings clientSettings =
         LanguageServiceSettings.newBuilder()
+            .setCredentialsProvider(fixedCredentialsProvider)
+            // .setQuotaProjectId()
             // .setTransportChannelProvider(transportChannelProvider)
-            .setHeaderProvider(new UserAgentHeaderProvider(this.getClass())) // change header
+            // .setHeaderProvider(new UserAgentHeaderProvider(this.getClass())) // change header
             .build();
     // Instantiates a client
     // LanguageServiceClient language = LanguageServiceClient.create();
     LanguageServiceClient language = LanguageServiceClient.create(clientSettings);
 
-    System.out.println("PRINT OUT HEADERS IN HEADERPROVIDER: ");
-    Map<String, String> headersMap = language.getSettings().getHeaderProvider().getHeaders();
-    headersMap.forEach((key, val) -> {
-      System.out.println(key + ": " + val +";");
+    Map<String, List<String>> requestMetadata = language.getSettings().getCredentialsProvider().getCredentials().getRequestMetadata();
+
+    LOGGER.info("getSettings().getCredentialsProvider().getCredentials().getRequestMetadata();");
+    requestMetadata.forEach((x, y) -> {
+      LOGGER.info(x + ": " + y + "\n");
     });
+    // System.out.println("PRINT OUT HEADERS IN HEADERPROVIDER: ");
+    // Map<String, String> headersMap = language.getSettings().getHeaderProvider().getHeaders();
+    // headersMap.forEach((key, val) -> {
+    //   System.out.println(key + ": " + val +";");
+    // });
     for (String text : this.texts) {
       Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
       // Detects the sentiment of the text
@@ -109,6 +148,8 @@ public class Controller {
           ", Sentiment: " + x.getSentiment() +
           ", Mention counts: " + x.getMentionsCount()));
     }
+
+    language.close();
   }
 
   @GetMapping("env")
