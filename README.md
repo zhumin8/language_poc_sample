71ac474a5d5f1f4ada9d9707f0123a0b79bdf79d
Min Zhu <zhumin@google.com>
2022-09-08 16:18:08 +0000

    add credentials demo. cleanup unused code.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 This is a quick sample app to try out the hand-written autoconfiguration classes.
 
 To run this app, download and install the autoconfiguration repo [here](https://github.com/zhumin8/language_poc1).
+
+- run the DemoApplication with `mvn spring-boot:run`
+- from a different terminal, try to call service with
+  - client library directly with `curl http://localhost:8080/nl`
+  - spring auto-config `curl http://localhost:8080/autoconfig`
+- check for client settings for both: `curl http://localhost:8080/printsettings`
--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,27 +1,25 @@
 package com.example.demo;
 
+import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.ExecutorProvider;
-import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.api.gax.core.InstantiatingExecutorProvider;
 import com.google.api.gax.retrying.RetrySettings;
-import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.auth.oauth2.UserCredentials;
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.Entity;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.LanguageServiceSettings;
 import com.google.cloud.language.v1.Sentiment;
-import com.google.cloud.spring.core.Credentials;
-import com.google.cloud.spring.core.UserAgentHeaderProvider;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ScheduledExecutorService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +29,7 @@ public class Controller {
   @Autowired
   private LanguageServiceClient autoClient;
 
+  private LanguageServiceClient languageClient;
   // The text to analyze
   private String[] texts = {"I love this!", "I hate this!",
       "Google, headquartered in Mountain View (1600 Amphitheatre Pkwy, Mountain View, CA 940430),"
@@ -45,22 +44,11 @@ public class Controller {
   @GetMapping("/printsettings")
   void printAutoconfigSettings() throws IOException {
 
-    String quotaProjectId = this.autoClient.getSettings().getQuotaProjectId();
-    Map<String, String> headers = this.autoClient.getSettings().getHeaderProvider().getHeaders();
-    Map<String, List<String>> requestMetadata = this.autoClient.getSettings().getCredentialsProvider().getCredentials().getRequestMetadata();
+    LOGGER.info("===========Spring auto-configuration Client Settings: ===========");
+    logSettingsForServiceClient(this.autoClient);
 
-    LOGGER.info("quotaProjectId used in service client: " + quotaProjectId + "\n");
-    LOGGER.info("this.autoClient.getSettings().getCredentialsProvider().getCredentials().getRequestMetadata();");
-    requestMetadata.forEach((x, y) -> {
-      LOGGER.info(x + ": " + y + "\n");
-    });
-    LOGGER.info("Headers");
-    headers.forEach((x, y) -> {
-      LOGGER.info(x + ": " + y + "\n");
-    });
-    InstantiatingExecutorProvider executorProvider = (InstantiatingExecutorProvider) this.autoClient.getSettings()
-        .getBackgroundExecutorProvider();
-    LOGGER.info("executorProvider thread count: " + executorProvider.getExecutorThreadCount());
+    LOGGER.info("===========Client Library Direct Setup Client Settings: ===========");
+    logSettingsForServiceClient(this.languageClient);
   }
 
   /**
@@ -70,7 +58,7 @@ public class Controller {
   @GetMapping("/autoconfig")
   void withAutoConfigClient() {
 
-    LOGGER.info("Calling Client Library with Spring auto-configuration.");
+    LOGGER.info("===========Calling Client Library with Spring auto-configuration.===========");
 
     callService(this.autoClient);
   }
@@ -80,9 +68,7 @@ public class Controller {
    */
   @GetMapping("/nl")
   void languageClientLib() throws IOException {
-    LOGGER.info("Calling Client Library directly.");
-
-    // TODO: add use service account
+    LOGGER.info("===========Calling Client Library directly.===============");
 
     ExecutorProvider executorProvider =
         LanguageServiceSettings.defaultExecutorProviderBuilder()
@@ -92,24 +78,28 @@ public class Controller {
     // set quota-project-id and executor-thread-count
     LanguageServiceSettings.Builder clientSettingsBuilder =
         LanguageServiceSettings.newBuilder()
-            .setQuotaProjectId("mzhu-test3")
+            // .setQuotaProjectId("project-id")
+            // for service account, alternatively:
+            // export GOOGLE_APPLICATION_CREDENTIALS=/path/to/my/key.json
+            .setCredentialsProvider(() -> ServiceAccountCredentials.fromStream(
+                new FileInputStream("/path/to/key.json")))
             .setBackgroundExecutorProvider(executorProvider);
     // add some retry settings here.
     RetrySettings retrySettings = clientSettingsBuilder
         .analyzeSentimentSettings()
-        .getRetrySettings();
-    retrySettings
+        .getRetrySettings()
         .toBuilder()
-        .setRetryDelayMultiplier(3.0);
+        .setRetryDelayMultiplier(3.0)
+        .build();
     clientSettingsBuilder
         .analyzeSentimentSettings()
         .setRetrySettings(retrySettings);
     // Instantiates a client
-    LanguageServiceClient language = LanguageServiceClient.create(clientSettingsBuilder.build());
+    this.languageClient = LanguageServiceClient.create(clientSettingsBuilder.build());
 
-    callService(language);
+    callService(this.languageClient);
 
-    language.close();
+    this.languageClient.close();
   }
 
   private void callService(LanguageServiceClient serviceClient) {
@@ -134,11 +124,47 @@ public class Controller {
     }
   }
 
+  private void logSettingsForServiceClient(LanguageServiceClient serviceClient) throws IOException {
+    String quotaProjectId = serviceClient.getSettings().getQuotaProjectId();
+    Map<String, String> headers = serviceClient.getSettings().getHeaderProvider().getHeaders();
+    Credentials credentials = serviceClient.getSettings().getCredentialsProvider().getCredentials();
+    // Map<String, List<String>> requestMetadata = serviceClient.getSettings().getCredentialsProvider().getCredentials().getRequestMetadata();
+
+    if (credentials instanceof UserCredentials) {
+      LOGGER.info("User credentials are being used.");
+      LOGGER.info("Client Id: " + ((UserCredentials) credentials).getClientId());
+      LOGGER.info("Quota Project Id: " + ((UserCredentials) credentials).getQuotaProjectId());
+    }
+    if (credentials instanceof ServiceAccountCredentials) {
+      LOGGER.info("Service account credentials are being used.");
+      LOGGER.info("Client Email: " + ((ServiceAccountCredentials) credentials).getClientEmail());
+      LOGGER.info("Project Id: " + ((ServiceAccountCredentials) credentials).getProjectId());
+
+    }
+    LOGGER.info("quotaProjectId used in service client: " + quotaProjectId + "\n");
+    // LOGGER.info("this.autoClient.getSettings().getCredentialsProvider().getCredentials().getRequestMetadata();");
+    // requestMetadata.forEach((x, y) -> {
+    //   LOGGER.info(x + ": " + y + "\n");
+    // });
+    InstantiatingExecutorProvider executorProvider = (InstantiatingExecutorProvider) serviceClient.getSettings()
+        .getBackgroundExecutorProvider();
+    LOGGER.info("executorProvider thread count: " + executorProvider.getExecutorThreadCount());
+
+    RetrySettings retrySettings = serviceClient.getSettings().analyzeSentimentSettings()
+        .getRetrySettings();
+    LOGGER.info("analyzeSentimentSettings - RetryDelayMultiplier: " + retrySettings.getRetryDelayMultiplier());
+
+    LOGGER.info("Headers");
+    headers.forEach((x, y) -> {
+      LOGGER.info(x + ": " + y + "\n");
+    });
+  }
+
   @GetMapping("env")
   String getEnv() {
     StringBuilder sb = new StringBuilder();
     System.getenv().forEach((k, v) -> {
-      if (k.equals("GOOGLE_CLOUD_PROJECT")) {
+      if (k.equals("GOOGLE_CLOUD_PROJECT") || k.equals("GOOGLE_APPLICATION_CREDENTIALS")) {
         sb.append(k + ":" + v + "\n");
       }
       // System.out.println(k + ":" + v);
